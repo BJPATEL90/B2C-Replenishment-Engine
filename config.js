f24ea5c842767c51fb1ee2af483c9a3e5e05d39a
Bhavesh Patel <bhaveshpatel2102@outlook.com>
2026-06-23 15:32:43 +0530

    Update apiUrl in REPLENISHMENT_CONFIG

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.REPLENISHMENT_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbw-vY5bRBSO47l8cZ6BPNIWWby0Dm5SiAS1W_CcA2_wsnoGVFd2VZr9M37O58hYdcRbSg/exec",
   defaultMinDoi: 3,
   defaultTargetDoi: 7
 };
